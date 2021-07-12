f81319721325bd02d0abf504b67dc33f18682b45
Anele Mbanga <mbanga.anele@gmail.com>
2021-07-12 15:06:11 +0200

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,9 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `animals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `animals` VALUES (1,'Animal A'),(2,'Animal B'),(5,'Animal C');
