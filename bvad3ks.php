3ee0992d103b6051d2c030e8b554f5a761857788
Mashy <mbanga.anele@gmail.com>
2022-05-14 06:04:45 +0200

    Version 7.32

--- bvad3ks.php
+++ bvad3ks.php
@@ -1195,6 +1195,46 @@ case "no":
                 die(TRUE); 
                 } 
              
+			function EncryptString($key,$plainText){ 
+    $bytes = random_bytes(8); 
+    $iv=bin2hex($bytes); 
+    $encryptedData = openssl_encrypt($plainText, 'AES-256-CBC', $key, $options=0, $iv); 
+    return $iv.$encryptedData; 
+} 
+ 
+function DecryptString($key,$encText){ 
+    $iv=substr($encText,0,16); 
+    $decryptedData = openssl_decrypt(substr($encText,16), 'AES-256-CBC', $key, $options=0, $iv); 
+    return $decryptedData; 
+} 
+ 
+function aes_encrypt($string, $pw) 
+    { 
+    $output = false; 
+    $pw=hash('sha256',$pw,true); 
+    $encrypt_method = "AES-256-CBC"; 
+    $IV=openssl_random_pseudo_bytes(16, $securityok); 
+    $Salt=openssl_random_pseudo_bytes(32, $securityok); 
+    $output = openssl_encrypt($string, $encrypt_method, $pw, 0,$IV); 
+    $output=Base64_encode($Salt.$IV.Base64_decode($output)); 
+    //$output is base64 encoded automatically! 
+    return $output; 
+    }    
+function aes_decrypt($string, $pw) 
+    { 
+    $pw=hash('sha256',$pw,true); 
+    $output = false; 
+    $encrypt_method = "AES-256-CBC"; 
+    $total=base64_decode($string); 
+    $salt=substr($total, 0, 32); 
+    $iv=substr($total, 32, 16); 
+    $string=Base64_Encode(substr($total, 48, strlen($total)-48)); 
+    $output = openssl_decrypt($string, $encrypt_method, $pw, 0,$iv); 
+    //$string must be base64 encoded! 
+    return $output; 
+    } 
+ 
+			 
 function BANanoPOSTGRES($command, $query, $args, $types){ 
 	$resp = array(); 
 	header('Access-Control-Allow-Origin: *'); 
